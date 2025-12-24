206126d4ef97a5ae014ca123302195819838f2e4
e3dr8nj <hy421725@gmail.com>
2025-12-24 03:26:03 +0300

    Updates

---
+++ _next/static/au0hVyqaEFU_CAtquyVUy/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/reactYoga//_next/:path+",
+        "destination": "/reactYoga/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
