aa17ead171d8ceea9e14f6e0d4a809a21516ff08
e3dr8nj <hy421725@gmail.com>
2025-12-25 01:20:36 +0300

    Update site

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-first-vite-react/', // Имя твоего репозитория на GitHub Pages
 })
